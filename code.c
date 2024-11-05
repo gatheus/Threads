c2b72144d4044cca499eb2129d56e48b5bd6360a
gatheus <gabrielmusico09@gmail.com>
2024-11-05 11:48:17 -0300

    Add files via upload

--- code.c
+++ code.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "funcoes.h" //Algumas funções que serão usadas
 #include "alocacao.h" //Para a alocação dinâmica
+#include "funcoesAux.h"
 
 void *OrdenacaoVetor(void *args) {
     struct timespec TempoInicial, TempoFinal;
@@ -65,52 +65,19 @@ void *OrdenacaoVetor(void *args) {
 } 
 
 int main(int argc, char *argv[]) {
-    pthread_t *Threads;
-    char **ArquivosEntrada;
-    char *ArquivoSaida;
-    parametros *p;
+    char **ArquivosEntrada = InicializarEntrada(argc, argv);
+    char *ArquivoSaida = InicializarSaida(argc, argv);
+    parametros *p = InicializarParametros(argc, argv);
+    pthread_t *Threads = AlocacaoDinamicaID(p->NumThreads);
     struct timespec inicio, fim;
-
-    p = AlocacaoDinamicaStruct(1);
-
-    //Obtendo o número de threads
-    p->NumThreads = atoi(argv[1]);
-    if (p->NumThreads!=1 && p->NumThreads!=2 && p->NumThreads!=4 && p->NumThreads!=8) {
-        printf("Numero de threads deve ser 2, 4 ou 8.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    /*Alocando dinamicamente a matriz dos arquivos de entrada
-    e vetores do arquivo de saída e de threads*/
-    ArquivosEntrada = (char**)malloc((argc-4)*sizeof(char*));
-    if (ArquivosEntrada == NULL) {
-        printf("Alocação falhou.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    for (int i=0; i<argc-4; i++) {
-        ArquivosEntrada[i] = AlocacaoDinamicaChar(100);
-    }
-    ArquivoSaida = AlocacaoDinamicaChar(100);
-    Threads = AlocacaoDinamicaID(p->NumThreads);
-
-    //Passando os dados de argv[] para vetores
-    for (int i=0; i<argc-4; i++) {
-        strcpy(ArquivosEntrada[i], argv[i+2]);
-    }
-    strcpy(ArquivoSaida, argv[argc-1]);
 
     /*Lendo os arquivos e registrando num vetor, e
     guardando também o tamanho desse vetor*/
     int *ContFinal = AlocacaoDinamicaInt(1);
     p->VetorNumeros = AbrirEContar(ArquivosEntrada, argc-4, ContFinal);
 
-    p->semaforo = AlocacaoDinamicaSemaforo(p->NumThreads);
-    p->InicioFim = AlocacaoDinamicaInt(2*(p->NumThreads));
-    p->TempoFinal = (struct timespec*)malloc((p->NumThreads)*sizeof(struct timespec));
-    p->TempoInicial = (struct timespec*)malloc((p->NumThreads)*sizeof(struct timespec));
-
-    //Iniciando semáforos
+    //Iniciando semáforos e mutex
+    pthread_mutex_init(&(p->mutex), NULL);
     sem_init(&(p->semaforo[(p->NumThreads)-1]), 0, 1);
     for (int i=0; i<p->NumThreads; i++) {
         sem_init(&(p->semaforo[i]), 0, 0);
@@ -118,42 +85,17 @@ int main(int argc, char *argv[]) {
 
     /*Vamos obter o valor do resto, e criar a variável "RestanteDoResto"
     para conseguirmos lidar com qualquer tamanho de vetor, divisível ou não*/
-    int resto = (*ContFinal)%(p->NumThreads);
-    int RestanteDoResto;
-    if (resto!=0) {
-        RestanteDoResto=resto;
-    }
-    else {
-        RestanteDoResto = -1;
-    }
-
-    pthread_mutex_init(&(p->mutex), NULL);
-    p->QualThreadEstou = 0;
+    int *RestanteDoResto = AlocacaoDinamicaInt(1);
+    int resto = CalcularResto(RestanteDoResto, *ContFinal, p);
 
     for (int i=0; i<p->NumThreads; i++) {
-        //Definindo o inicio e o fim de cada thread
-        //Obs: posições pares definem os inícios, e ímpares, o fim
-        int razao = (*ContFinal)/(p->NumThreads);
-        p->InicioFim[2*i] = i*razao;
-        p->InicioFim[(2*i)+1] = ((i+1)*razao)-1;
-
-        //Para caso o tamanho do vetor nao seja divisivel pelo numero de threads:
-        if (RestanteDoResto==0) {
-                p->InicioFim[2*i]=(p->InicioFim[(2*(i-1))+1])+1;
-                p->InicioFim[2*i+1]=p->InicioFim[2*i]+razao-1;
-            }
-        if (RestanteDoResto>0) {
-            p->InicioFim[(2*i)+1] += (resto-RestanteDoResto+1); 
-            p->InicioFim[2*i] += (resto-RestanteDoResto);
-            RestanteDoResto--;
-        } 
+        DefinirInicioFim(i, p, resto, RestanteDoResto, *ContFinal);
 
         //criando a Thread
         if (i==0)
             clock_gettime(CLOCK_MONOTONIC, &inicio);
 
-        int result = pthread_create(&Threads[i], NULL, OrdenacaoVetor, p);
-        if (result!=0) {
+        if (pthread_create(&Threads[i], NULL, OrdenacaoVetor, p)) {
             printf("Erro ao criar a thread %d\n", i+1);
             exit(EXIT_FAILURE);
         }
@@ -164,41 +106,17 @@ int main(int argc, char *argv[]) {
         pthread_join(Threads[i], NULL);
     }  
     clock_gettime(CLOCK_MONOTONIC, &fim);
-    double tempoTotal;
-    tempoTotal = (fim.tv_sec - inicio.tv_sec); // segundos
-    tempoTotal += (fim.tv_nsec - inicio.tv_nsec) / 1e9; // nanosegundos
 
-    double tempoCadaThread;
-    for (int i=0; i<p->NumThreads; i++) {
-        tempoCadaThread = ((p->TempoFinal[i]).tv_sec - (p->TempoInicial[i]).tv_sec); // segundos
-        tempoCadaThread += ((p->TempoFinal[i]).tv_nsec - (p->TempoInicial[i]).tv_nsec) / 1e9; // nanosegundos
-        printf("Tempo de execução da %d thread: %f segundos.\n", i, tempoCadaThread);
-    } 
-
-    printf("Tempo total de execução: %f\n", tempoTotal);
+    //Obtendo tempos  de cada thread e tempo total
+    ExibirTempo(inicio, fim, p);
 
     //Registrando saída
     RegistrarArqSaida(ArquivoSaida, p->VetorNumeros, *ContFinal);
 
     //Liberando tudo que usamos e não precisamos mais
-    for (int i=0; i<p->NumThreads; i++) {
-        sem_destroy(&(p->semaforo[i]));
-    }
-    pthread_mutex_destroy(&(p->mutex));
-
-    for (int i=0; i<argc-4; i++) {
-        free(ArquivosEntrada[i]);
-    }
-    free(ArquivosEntrada);
-    free(ArquivoSaida);
+    LiberarMemoria(p, ArquivosEntrada, ArquivoSaida, argc);
     free(Threads);
-    free(p->VetorNumeros);
-    free(p->semaforo);
-    free(p->InicioFim);
-    free(p->TempoInicial);
-    free(p->TempoFinal);
     free(ContFinal);
-    free(p);
 
     return 0;
 }
